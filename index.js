c00c4172eeb5837084d5e7a5e00cda1e9213821e
Francisco ferreira <francisco170142329@gmail.com>
2020-08-13 21:38:18 -0500

    configuring the nodemon

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const server = express();
 
 server.get('/t', (req,res) => {
-    return res.send('welcome!');
+    return res.json({
+        curso: 'Nodejs'
+    })
 });
 
 server.listen(3000);
