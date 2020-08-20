a35b38f60d87d6cb13e532fbfd484ac374e72590
Francisco <francisco170142329@gmail.com>
2020-08-19 23:48:29 -0300

    Criando o CRUD

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ const express = require("express");
 
 const server = express();
 
-server.get('/t', (req,res) => {
-    return res.json({
-        curso: 'Nodejs'
-    })
+server.use(express.json());
+
+const cursos = ['nodejs', 'php','react'];
+
+server.get('/cursos:index', (req,res) => {
+    return res.json(cursos);
+});
+
+server.post('/cursos', (req, res) => {
+    const { nome } = req.body;
+    cursos.push(nome);
+
+    return  res.json(cursos);
+});
+
+server.put('/cursos/:index', (req,res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+})
+
+server.delete('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+
+    cursos.splice(index, 1);
+
+    return res.json(cursos);
+})
+
+
+server.get('/', (req, res) => {
+    return res.json("Welcome")
 });
 
 server.listen(3000);
