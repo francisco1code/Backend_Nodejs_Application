e9954c5e595e4dad90f3af4c5eb20167680be1df
Francisco <francisco170142329@gmail.com>
2020-08-23 00:24:08 -0300

    Criando os primeiros middlewares

--- index.js
+++ index.js
@@ -6,18 +6,32 @@ server.use(express.json());
 
 const cursos = ['nodejs', 'php','react'];
 
+//Middleware Global
+server.use((req, res, next) => {
+    console.log(`URM CHAMDA: ${req.url}`);
+    return next();
+})
+
+function checkCursos(req,res, next){
+    if(!req.body.name){
+        return res.status(400).json({ error: "Nome é Obrigatorio"});
+    }
+    return next();
+}
+
+
 server.get('/cursos:index', (req,res) => {
     return res.json(cursos);
 });
 
-server.post('/cursos', (req, res) => {
+server.post('/cursos',checkCursos, (req, res) => {
     const { nome } = req.body;
     cursos.push(nome);
 
     return  res.json(cursos);
 });
 
-server.put('/cursos/:index', (req,res) => {
+server.put('/cursos/:index',checkCursos, (req,res) => {
     const { index } = req.params;
     const { name } = req.body;
 
